caac6c8b13f8d8fc60c4dbafd51723694fe86a5c
Joaquin Diaz <joaquin_fdv@goshippo.com>
2019-08-29 19:05:15 -0300

    Use API token in requests

--- app/film_locations_service/api.py
+++ app/film_locations_service/api.py
@@ -2,7 +2,14 @@ import requests
 
 class FilmLocationsAPI(object):
   url = "https://data.sfgov.org/resource/wwmu-gmzc.json"
+  # TODO: API tokens should not be commited to the code
+  api_token = "IegozTH7xlN32jXMfFpqR8x1K"
 
-  def fetch_film_locations(self, query = "", limit = 100):
-    response = requests.get(self.url, params={"$q": query, "$limit": limit})
+  def _get_headers(self):
+    return {
+      "X-App-Token": self.api_token
+    }
+
+  def fetch_film_locations(self, query = "", limit = 10):
+    response = requests.get(self.url, params={"$q": query, "$limit": limit}, headers=self._get_headers())
     return response.json()
